13f41b03c526d2f156563a795ca1000bf9d4b0d6
Gamitha <gamithamanawadu26@gmail.com>
2020-10-09 20:00:10 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import Row from "./Row";
 import requests from "./Requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      {}
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"

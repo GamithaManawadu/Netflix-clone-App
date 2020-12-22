edbdacddba40f5c60219ba4726f934a9fdc17175
Gamitha <gamithamanawadu26@gmail.com>
2020-12-22 18:44:33 +0530

    changes

--- src/Banner.js
+++ src/Banner.js
@@ -48,7 +48,7 @@ function Banner() {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
-      <div className="banner-fadeBottom"></div>
+      <div className="banner-fadeBottom"/>
     </header>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+useEffect(() => {
+  window.addEventListener("scroll",()=>{
+    if (window.scrollY > 100){
+        handleShow(true);
+    }else handleShow(false);
+  });
+
+  return () => {
+    window.removeEventListener("scroll");
+  };
+
+}, []);
+  
+  
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="/iconfinder_Pinterest_UI-18_2540590.png"
         alt="Netflix Logo"
       />
     </div>
